75662805076e1a991aa995ecd3d383637c5e9e7e
Leuson <leusonmario@gmail.com>
2017-06-29 15:26:21 -0300

    New case of declaration duplication

--- lib/BuildConflictExtractor/BCTypes/BCStatementDuplication.rb
+++ lib/BuildConflictExtractor/BCTypes/BCStatementDuplication.rb
@@ -11,31 +11,37 @@ class BCStatementDuplication
 		print filesConflicting[count][2]
 
 		while(count < filesConflicting.size)
-			if(baseRight[0][filesConflicting[count][0]] != nil and baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/))
-				internalCount = 0
-				methodOccurence = baseRight[0][filesConflicting[count][0]].to_s.to_enum(:scan, /Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/).map { Regexp.last_match }
-				while(internalCount < methodOccurence.size)
-					nodeMethod = methodOccurence[count].to_s.split("(").last.gsub(')','')
-					print nodeMethod
-					print "\n"
-					print baseLeft[0][filesConflicting[count][0]]
-					if (baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}[0-9\(\)]* into [a-zA-Z]*\(#{nodeMethod}\)/) and baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}\([0-9]*\) into /))
-						return true
+			if (filesConflicting[count][1] != "method")
+				if(baseRight[0][filesConflicting[count][0]] != nil and baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/))
+					internalCount = 0
+					methodOccurence = baseRight[0][filesConflicting[count][0]].to_s.to_enum(:scan, /Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/).map { Regexp.last_match }
+					while(internalCount < methodOccurence.size)
+						nodeMethod = methodOccurence[count].to_s.split("(").last.gsub(')','')
+						print nodeMethod
+						print "\n"
+						print baseLeft[0][filesConflicting[count][0]]
+						if (baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}[0-9\(\)]* into [a-zA-Z]*\(#{nodeMethod}\)/) and baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}\([0-9]*\) into /))
+							return true
+						end
+						internalCount += 1
 					end
-					internalCount += 1
 				end
-			end
-			if(baseLeft[0][filesConflicting[count][0]] != nil and baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/))
-				internalCount = 0
-				methodOccurence = baseLeft[0][filesConflicting[count][0]].to_s.to_enum(:scan, /Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/).map { Regexp.last_match }
-				while(internalCount < methodOccurence.size)
-					nodeMethod = methodOccurence[count].to_s.split("(").last.gsub(')','')
-					print nodeMethod
-					print "\n"
-					if (baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}[0-9\(\)]* into [a-zA-Z]*\(#{nodeMethod}\)/) and baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}\([0-9]*\) into /))
-						return true
+				if(baseLeft[0][filesConflicting[count][0]] != nil and baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/))
+					internalCount = 0
+					methodOccurence = baseLeft[0][filesConflicting[count][0]].to_s.to_enum(:scan, /Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/).map { Regexp.last_match }
+					while(internalCount < methodOccurence.size)
+						nodeMethod = methodOccurence[count].to_s.split("(").last.gsub(')','')
+						print nodeMethod
+						print "\n"
+						if (baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}[0-9\(\)]* into [a-zA-Z]*\(#{nodeMethod}\)/) and baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][1]}\([0-9]*\) into /))
+							return true
+						end
+						internalCount += 1
 					end
-					internalCount += 1
+				end
+			else
+				if(baseLeft[0][filesConflicting[count][0]] != nil and baseLeft[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/) and baseRight[0][filesConflicting[count][0]] != nil and baseRight[0][filesConflicting[count][0]].to_s.match(/Insert SimpleName: #{filesConflicting[count][2]}[0-9\(\)]* into [a-zA-Z]*[0-9\(\)]*/))
+					return true
 				end
 			end
 			count += 1
--- lib/CausesExtractor/IndividualExtractor/StatementDuplicationExtractor.rb
+++ lib/CausesExtractor/IndividualExtractor/StatementDuplicationExtractor.rb
@@ -5,13 +5,14 @@ class StatementDuplicationExtractor
 	end
 
 	def extractionFilesInfo(buildLog)
+		#buildLog = build.match(/[\s\S]* BUILD FAILURE/)
 		filesInformation = []
-		numberOccurrences = buildLog.scan(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\s]* is already defined in [a-zA-Z0-9\/\-\.\:\[\]\,\_]*/).size
+		numberOccurrences = buildLog.scan(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\(\)\s]* is already defined in [a-zA-Z0-9\/\-\.\:\[\]\,\_]*/).size
 		begin
-			information = buildLog.to_enum(:scan, /\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\s]* is already defined in [a-zA-Z0-9\/\-\.\:\[\]\,\_]*/).map { Regexp.last_match }
+			information = buildLog.to_enum(:scan, /\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\(\)\s]* is already defined in [a-zA-Z0-9\/\-\.\:\[\]\,\_]*/).map { Regexp.last_match }
 			count = 0
 			while(count < information.size)
-				classFile = information[count].to_s.match(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\s]*.java/)[0].split("/").last.gsub('.java','')
+				classFile = information[count].to_s.match(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\]\,\s]*.java:/)[0].split("/").last.gsub('.java:','')
 				variableName = information[count].to_s.match(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\,]*\]\s[a-zA-Z0-9\/\-\_]*/)[0].split(" ").last
 				methodName = information[count].to_s.match(/\[ERROR\] [a-zA-Z0-9\/\-\.\:\[\,\]\s\_]*/)[0].split(" ").last
 				count += 1
