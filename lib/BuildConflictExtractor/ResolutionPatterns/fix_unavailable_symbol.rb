c5d1b89f624ee20b341efb7bce5944d196c390c0
Leuson <leusonmario@gmail.com>
2017-08-03 15:19:08 -0300

    Import update on change pattern of BC fix

--- lib/BuildConflictExtractor/ResolutionPatterns/fix_unavailable_symbol.rb
+++ lib/BuildConflictExtractor/ResolutionPatterns/fix_unavailable_symbol.rb
@@ -7,8 +7,10 @@ class FixUnavailableSymbol
     index = 0
     fixPattern = []
     filesConflictsInfo.each do |fileConflictBuild|
-      if (diffErrorFix[0][fileConflictBuild[0]].to_s.match(/Insert QualifiedName: [\s\S]*#{fileConflictBuild[1]}[\s\S]* into ImportDeclaration/))
+      if (diffErrorFix[0][fileConflictBuild[0]].to_s.match(/Insert QualifiedName: [\s\S]*#{fileConflictBuild[1]}[\s\S]* into ImportDeclaration/) or diffErrorFix[0][fileConflictBuild[0]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\(\) ]*\.#{fileConflictBuild[1]}/))
         fixPattern[index] = "IMPORT-UPDATE"
+      elsif (diffErrorFix[0][fileConflictBuild[0]].to_s.match(/Delete QualifiedName: [\s\S]*#{fileConflictBuild[1]}/))
+        fixPattern[index] = "DELETE-IMPORT"
       elsif (diffErrorFix[0][fileConflictBuild[0]].to_s.match(/Update SimpleName: [\s\S]*#{fileConflictBuild[1]}[\s\S]* to [\s\S]*\n/) or diffErrorFix[0][fileConflictBuild[2]].to_s.match(/Update SimpleName: [\s\S]*#{fileConflictBuild[1]}[\s\S]* to [\s\S]*\n/))
           fixPattern[index] = "METHOD-NAME-UPDATE"
       elsif (diffErrorFix[0][fileConflictBuild[2]].to_s.match(/Delete QualifiedName: [\s\S]*#{fileConflictBuild[1]}(\n)?/))
