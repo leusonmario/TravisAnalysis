d5473a0b0ab47634aed36c0832d71a53e143ef4a
lhns <lhns@cin.ufpe.br>
2018-10-05 15:44:08 -0300

    Update regex from fix_unavailable_symbol to get more patterns of update

--- lib/BuildConflictExtractor/ResolutionPatterns/fix_unavailable_symbol.rb
+++ lib/BuildConflictExtractor/ResolutionPatterns/fix_unavailable_symbol.rb
@@ -30,12 +30,13 @@ class FixUnavailableSymbol
       else
         if (diffErrorFix[0].size == 1 and diffErrorFix[0]['pom.xml'] != nil )
           return "DEPENDENCY-CHANGE"
-        elsif (diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\_]*#{filesConflictsInfo[2]}[\(\)0-9]* to [a-zA-Z0-9\.\_]*#{filesConflictsInfo[1]}/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Insert QualifiedName: [\s\S]*#{filesConflictsInfo[2]}[\s\S]* into ImportDeclaration/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\(\) ]*\.#{filesConflictsInfo[2]}/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\(\) ]*/))
+        elsif (diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/(Update|Insert) QualifiedName: [a-zA-Z0-9\.\_]*#{filesConflictsInfo[2]}[\(\)0-9]* to [a-zA-Z0-9\.\_]*#{filesConflictsInfo[1]}/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Insert QualifiedName: [\s\S]*#{filesConflictsInfo[2]}[\s\S]* into ImportDeclaration/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\(\) ]*\.#{filesConflictsInfo[2]}/) or diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Update QualifiedName: [a-zA-Z0-9\.\(\) ]*/))
           return "IMPORT-UPDATE"
         elsif (diffErrorFix[0][filesConflictsInfo[1]].to_s.match(/Delete (QualifiedName:|SimpleName:SimpleType:) [\s\S]*#{filesConflictsInfo[2]}/))
             return "DELETE-IMPORT"
         elsif (diffErrorFix[0][filesConflictsInfo[3]].to_s.match(/Delete QualifiedName: [\s\S]*#{filesConflictsInfo[2]}(\n)?/) or diffErrorFix[0][filesConflictsInfo[3]].to_s.match(/Delete SimpleName: #{filesConflictsInfo[2]}[\(\)0-9]*/))
           return "MISSING-ELEMENT-REMOVAL"
+
         end
       end
       return "OTHER"
